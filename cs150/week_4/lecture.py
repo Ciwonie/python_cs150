5c82cf46bb27405455a10cb4d43b5984c8e7b44a
Jonathan Cirone <jonathan@Jonathans-MacBook-Pro.local>
2019-02-15 00:56:23 -0600

    updates to plugins

--- cs150/week_4/lecture.py
+++ cs150/week_4/lecture.py
@@ -4,22 +4,22 @@ facultySet = []
 gpaSet = []
 researchTopicSet = []
 
-with open('studentRecord.csv') as csv_file :
-    for count, line in enumerate(csv_file) :
-        if count == 0 :
+with open('studentRecord.csv') as csv_file:
+    for count, line in enumerate(csv_file):
+        if count == 0:
             continue
         lineValues = line.rstrip('\n').strip('-').split(',')
         # The following two lines will show how lineVariable works:
         # print("lineValues Variable : " + str(lineValues))
         # print("count: " + str(count))
-        for index, word in enumerate(lineValues) :
-            if index == 0 :
+        for index, word in enumerate(lineValues):
+            if index == 0:
                 studentSet.append(word)
-            elif index == 1 :
+            elif index == 1:
                 gpaSet.append(float(word))
-            elif index == 2 :
+            elif index == 2:
                 facultySet.append(word)
-            else :
+            else:
                 researchTopicSet.append(word)
 
 print(studentSet)
@@ -31,25 +31,26 @@ print(researchTopicSet)
 students = []
 facultyMembers = []
 
-class Student :
+
+class Student:
 
     studentCount = 0
     gpaSum = 0
 
-    #constructor function - creates the object (instance)
-    def __init__(self, name) :
+    # constructor function - creates the object (instance)
+    def __init__(self, name):
         self.studentName = name
         Student.studentCount = Student.studentCount + 1
 
-    def setGPA(self, gpa) :
+    def setGPA(self, gpa):
         self.studentGPA = gpa
         Student.gpaSum += gpa
 
 
-for index, item in enumerate(studentSet) :
+for index, item in enumerate(studentSet):
     newStudent = Student(item)
     newStudent.setGPA(gpaSet[index])
-    
+
     print(newStudent.studentName, newStudent.studentGPA)
 
 print('The number of students ' + str(Student.studentCount))
