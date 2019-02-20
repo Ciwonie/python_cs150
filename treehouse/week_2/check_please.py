6f03aecab99ff36c657f7e29f23a684cae3a2d6a
Jonathan Cirone <jonathan@Jonathans-MacBook-Pro.local>
2019-02-20 01:29:46 -0600

    practice with try and raise commands

---
+++ treehouse/week_2/check_please.py
@@ -0,0 +1,19 @@
+# this program is designed to test functions and use try/raise commands to control the input of data from a user
+import math
+
+
+def split_check(total, number_of_people):
+    if number_of_people <= 1:
+        raise ValueError("More than 1 person is required to split the check")
+    return math.ceil(total / number_of_people)
+
+
+try:
+    total_due = float(input('What is the total bill? '))
+    number_of_people = int(input('How many people? '))
+    amount_due = split_check(total_due, number_of_people)
+except ValueError as err:
+    print("Oh no! That's not a valid value. Try again...")
+    print("({})".format(err))
+else:
+    print('Each person owes ${}'.format(amount_due))
