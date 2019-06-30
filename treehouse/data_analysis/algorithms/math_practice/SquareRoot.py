32293464947c712903892a3d42a77f012b69b007
Jonathan Cirone <jonathan@Jonathans-MacBook-Pro.local>
2019-06-30 16:31:41 -0500

    Added additional education sources;

--- treehouse/data_analysis/algorithms/math_practice/SquareRoot.py
+++ treehouse/data_analysis/algorithms/math_practice/SquareRoot.py
@@ -1,3 +1,9 @@
 # This algorithm is designed to guess the square root of any number
 # First, enter the number tou want rooted
 # Secondanly, input your guess of the first number's root
+
+import os
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
